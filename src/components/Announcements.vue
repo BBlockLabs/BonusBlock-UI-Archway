0b1a870ee06af70aee147ebc0460d7eae65b313c
Deniss <deniss@altermail.co>
2023-04-26 16:11:16 +0300

    announcements ui

--- src/components/Announcements.vue
+++ src/components/Announcements.vue
@@ -1,121 +1,133 @@
 <template>
-  <template v-for="announcement in announcements" :key="announcement.title">
-    <el-col :span="12" :xl="24 / 3" :xs="24" class="announcement-card">
-      <el-card shadow="never">
-        <img
-          :src="announcement.image"
-          class="image"
-          :alt="announcement.title"
-        />
-        <el-row justify="center" align="top">
-          <el-col :span="24" class="p-medium">
-            <h4 class="m-0">
-              {{ announcement.title }}
-            </h4>
-            <span v-html="announcement.description" />
-            <el-row class="pt-medium socials">
-              <el-col :span="18">
-                <template
-                  v-for="social in announcement.socials"
-                  :key="social.type"
-                >
-                  <el-link
-                    :href="social.link || social.url"
-                    target="_blank"
-                    :underline="false"
-                    class="mr-small mt-small"
-                  >
-                    <FontAwesomeIcon
-                      v-if="social.type === 'twitter'"
-                      icon="fa-brands fa-twitter"
-                      size="xl"
-                    />
-                    <FontAwesomeIcon
-                      v-else-if="social.type === 'telegram'"
-                      icon="fa-brands fa-telegram"
-                      size="xl"
-                    />
-                    <FontAwesomeIcon
-                      v-else-if="social.type === 'youtube'"
-                      icon="fa-brands fa-youtube"
-                      size="xl"
-                    />
-                    <FontAwesomeIcon
-                      v-else-if="social.type === 'discord'"
-                      icon="fa-brands fa-discord"
-                      size="xl"
-                    />
-                    <FontAwesomeIcon
-                      v-else-if="social.type === 'reddit'"
-                      icon="fa-brands fa-reddit"
-                      size="xl"
-                    />
-                    <FontAwesomeIcon
-                      v-else-if="social.type === 'icon'"
-                      :icon="social.icon"
-                      size="xl"
-                    />
-                  </el-link>
-                </template>
-                <social-links
-                  :twitter="announcement.twitterLink"
-                  :discord="announcement.discordLink"
-                  :youtube="announcement.youtubeLink"
-                  :telegram="announcement.telegramLink"
-                  :reddit="announcement.redditLink"
-                />
-              </el-col>
-              <el-col :span="6" class="align-right">
-                <el-link
-                  target="_blank"
-                  :href="announcement.mainLink"
-                  :underline="false"
-                >
-                  <el-button v-if="announcement.mainLink" type="primary">
-                    {{ announcement.mainLinkTitle }}
-                  </el-button>
-                </el-link>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
-      </el-card>
-    </el-col>
-  </template>
+  <el-col
+    v-for="announcement in announcements"
+    :key="announcement.title"
+    ref="announcementBox"
+    :span="12"
+    :xl="24 / 3"
+    :xs="24"
+    class="announcement-card"
+  >
+    <el-card shadow="never">
+      <img :src="announcement.image" class="image" :alt="announcement.title" />
+
+      <div class="announcement-text pr-medium pl-medium pt-small">
+        <h3 class="m-0 pb-small">
+          {{ announcement.title }}
+        </h3>
+
+        <span v-html="announcement.description" />
+      </div>
+      <el-row class="socials pr-medium pl-medium">
+        <el-col :span="18">
+          <template v-for="social in announcement.socials" :key="social.type">
+            <el-link
+              :href="social.link || social.url"
+              target="_blank"
+              :underline="false"
+              class="mr-small mt-small"
+            >
+              <FontAwesomeIcon
+                v-if="social.type === 'twitter'"
+                icon="fa-brands fa-twitter"
+                size="xl"
+              />
+              <FontAwesomeIcon
+                v-else-if="social.type === 'telegram'"
+                icon="fa-brands fa-telegram"
+                size="xl"
+              />
+              <FontAwesomeIcon
+                v-else-if="social.type === 'youtube'"
+                icon="fa-brands fa-youtube"
+                size="xl"
+              />
+              <FontAwesomeIcon
+                v-else-if="social.type === 'discord'"
+                icon="fa-brands fa-discord"
+                size="xl"
+              />
+              <FontAwesomeIcon
+                v-else-if="social.type === 'reddit'"
+                icon="fa-brands fa-reddit"
+                size="xl"
+              />
+              <FontAwesomeIcon
+                v-else-if="social.type === 'icon'"
+                :icon="social.icon"
+                size="xl"
+              />
+            </el-link>
+          </template>
+        </el-col>
+        <el-col :span="6" class="align-right">
+          <el-link
+            target="_blank"
+            :href="announcement.mainLink"
+            :underline="false"
+          >
+            <el-button v-if="announcement.mainLink" type="primary">
+              {{ announcement.mainLinkTitle }}
+            </el-button>
+          </el-link>
+        </el-col>
+      </el-row>
+    </el-card>
+  </el-col>
 </template>
-<script setup lang="ts">
-import SocialLinks from "@/components/SocialLinks.vue";
-import { onMounted, ref } from "vue";
-import AnnouncementsDto from "@/common/api/dto/AnnouncementsDto";
-import { StoreType, useStore } from "@/store";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+<script>
+import { ref } from "vue";
+import { useStore } from "@/store";
 
-const store: StoreType = useStore();
+export default {
+  setup() {
+    const announcements = ref({});
+    const announcementBox = ref(null);
 
-let announcements = ref(Array<AnnouncementsDto>());
-
-onMounted(async () => {
-  try {
-    announcements.value = await store.dispatch(
+    return {
+      announcements,
+      announcementBox,
+    };
+  },
+  async created() {
+    const store = useStore();
+    this.announcements = await store.dispatch(
       "HttpModule/getAnnouncementsList"
     );
-    console.log(announcements.value);
-  } catch (e) {
-    console.error(e);
-  }
-});
+
+    this.$nextTick(() => {
+      let maxH = 0;
+      for (const elem in this.announcementBox) {
+        maxH = Math.max(
+          maxH,
+          this.announcementBox[elem].$el.querySelector(".announcement-text")
+            .clientHeight
+        );
+      }
+      for (const elem in this.announcementBox) {
+        this.announcementBox[elem].$el.querySelector(
+          ".announcement-text"
+        ).style.height = maxH + "px";
+      }
+    });
+  },
+};
 </script>
 <style lang="scss">
 .announcement-card {
   margin-bottom: 1em;
   max-width: 32em;
 
+  .el-card {
+    height: 100%;
+  }
+
   .el-card__body {
     padding: 0;
 
     .image {
       width: 100%;
-      height: 20%;
+      height: 45%;
       object-fit: cover;
       aspect-ratio: 16/9;
     }
