cf736ecfc02fc023509ad6b0a0d5b7619e017115
Anton Tokalov <anton@altermail.co>
2023-07-26 18:44:08 +0300

    BBLOCK-386: add /leaderboard endpoint call

---
+++ src/common/api/archway/ArchwayLeaderboardRecordDto.ts
@@ -0,0 +1,6 @@
+export default class ArchwayLeaderboardRecordDto {
+  walletAddress: string = "";
+  topDapp: string = "";
+  score: number = 0;
+  totalOnChain: number = 0;
+}
---
+++ src/common/api/archway/ArchwayLeaderboardResponse.ts
@@ -0,0 +1,6 @@
+import type ArchwayLeaderboardRecordDto from "@/common/api/archway/ArchwayLeaderboardRecordDto";
+
+export default class ArchwayLeaderboardResponse {
+  totalRows: number = 0;
+  searchResults: Array<ArchwayLeaderboardRecordDto> = [];
+}
