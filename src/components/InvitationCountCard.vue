25d1b12cddf98f2946facb97c9aff5133477a32b
Franco Bouzo <franco@altermail.co>
2023-09-07 04:11:05 +0300

    BBLOCK-468: Fixed invitation count card

--- src/components/InvitationCountCard.vue
+++ src/components/InvitationCountCard.vue
@@ -1,15 +1,14 @@
 <template>
   <el-card>
     <el-row justify="center">
-      <el-col :span="-1"> Successful Invitations </el-col>
-    </el-row>
-
-    <el-row justify="center">
       <el-col :span="-1">
         <h1 class="title my-medium">
           {{ store.state.UserModule?.user?.invitedCount || "0" }}
         </h1>
       </el-col>
+    </el-row>
+    <el-row justify="center">
+      <el-col :span="-1"><h3>Successful Invitations</h3></el-col>
     </el-row>
   </el-card>
 </template>
